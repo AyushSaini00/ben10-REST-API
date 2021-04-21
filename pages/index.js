1924ea2cfc863374579c18d0d0bc34614f9c3988
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-04-21 18:55:15 +0530

    Improved: index.js

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,21 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <section className="section-home">
-    <div className="home">
-      <h1>A RESTful Ben 10 API</h1>
-      <h3>Get hand curated Ben 10 data</h3>
-      <p>
-        Powered by <span className="highlight">Express</span> & <span className="highlight">MongoDB</span> 
-      </p>
+    <div>
+      <section className="section-home">
+        <div className="home">
+          <h1>A RESTful Ben 10 API</h1>
+          <h3>Get hand curated Ben 10 data</h3>
+          <p>
+            Powered by <span className="highlight">Express</span> & <span className="highlight">MongoDB</span> 
+          </p>
+        </div>
+        <Link href="/docs">
+          <a className="btn">
+            GET STARTED &nbsp; →
+          </a>
+        </Link>
+      </section>
     </div>
-    <Link href="/docs">
-      <a className="btn">
-        GET STARTED &nbsp; →
-      </a>
-    </Link>
-    </section>
   )
 }
