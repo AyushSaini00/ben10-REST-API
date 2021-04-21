32d67fd79c0e22bcc12ed42dae046444a193552f
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-04-21 20:41:51 +0530

    Added Page: sponsers

---
+++ pages/sponsers.js
@@ -0,0 +1,13 @@
+const Sponsers = () => {
+    return (
+        <div>
+            <section className="section-page">
+                <div className="section-page-div">
+                    <h2 className="page-title">Sponsers</h2>
+                </div>
+            </section>
+        </div>
+    );
+}
+ 
+export default Sponsers;
