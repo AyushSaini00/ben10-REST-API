25da536c0b2e7d42795d0073aaae039f180b4cf3
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-04-28 09:11:07 +0530

    :mag: Component: Metadata

---
+++ components/Metadata.js
@@ -0,0 +1,51 @@
+import Head from "next/head";
+
+const Metadata = ({ title, description }) => {
+  return (
+    <Head>
+      {/* BASIC */}
+
+      <title>{title ? title : "Ben 10 RESTful API"}</title>
+      <meta
+        name="description"
+        content={
+          description
+            ? description
+            : "Ben 10 API is a RESTful Api serving hand curated Ben 10 Alien data"
+        }
+      />
+      <meta
+        name="keywords"
+        content="ben10api, restapi, restful api, ben10, express, nodejs, ben 10"
+      />
+      <meta name="author" content="Ayush Saini" />
+      {/* ESSENTIALS */}
+
+      <meta property="og:title" content={title ? title : "Ben 10 RESTful API"} />
+      <meta
+        property="og:description"
+        content={
+          description
+            ? description
+            : "Ben 10 API is a RESTful Api serving hand curated Ben 10 Alien data"
+        }
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Ben 10 API" />
+
+      {/* NON-ESSENTIALS */}
+      <meta name="twitter:site" content="@ben10api" />
+      <meta name="twitter:creator" content="@ben10api" />
+      <meta
+        name="twitter:description"
+        content={
+          description
+            ? description
+            : "Ben 10 API is a RESTful Api serving hand curated Ben 10 Alien data"
+        }
+      ></meta>
+    </Head>
+  );
+};
+
+export default Metadata;
