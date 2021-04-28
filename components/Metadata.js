d98e59214366607ed07d695187c3bf36b95151fb
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-04-28 09:27:00 +0530

    :art: Moved bmac script

--- components/Metadata.js
+++ components/Metadata.js
@@ -44,6 +44,19 @@ const Metadata = ({ title, description }) => {
             : "Ben 10 API is a RESTful Api serving hand curated Ben 10 Alien data"
         }
       ></meta>
+
+      <script
+        data-name="BMC-Widget"
+        data-cfasync="false"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        data-id="ayushsaini"
+        data-description="Support me on Buy me a coffee!"
+        data-message=""
+        data-color="#5F7FFF"
+        data-position="Right"
+        data-x_margin="18"
+        data-y_margin="18"
+      ></script>
     </Head>
   );
 };
